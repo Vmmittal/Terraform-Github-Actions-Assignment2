4a96a0589ba8b36b23a3685376e048aa1963e2bb
Vipul Kumar <vipulmittal48@gmail.com>
2023-05-24 08:40:11 +0530

    Added Subnet

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-0889a44b331db0194"
   instance_type = "t2.micro"
-
+  subnet_id = "subnet-0ee633ea9e4e4fb04" 
   tags = {
     Name = "My-ec2-github"
     Owner="vipul.kumar@cloudeq.com"
